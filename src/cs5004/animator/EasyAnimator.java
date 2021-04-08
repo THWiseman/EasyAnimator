b6155ad7bb17da468fe88d6b216c0a3b3977043d
Tom W <papahsquat@gmail.com>
2021-04-08 12:51:34 -0400

    started work on the swing view.

--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -6,10 +6,18 @@ import cs5004.animator.model.Rectangle;
 import cs5004.animator.util.AnimationReader;
 import cs5004.animator.view.SwingView;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public final class EasyAnimator {
   public static void main(String[] args) {
-    Readable file
-    Canvas canvas = AnimationReader.parseFile(toh-3.txt);
+    Readable file;
+    try {
+      file = new FileReader("toh-3.txt");
+    } catch (FileNotFoundException e) {
+      throw new IllegalArgumentException("File not found");
+    }
+    Canvas canvas =  AnimationReader.parseFile(file, new CanvasImpl.Builder);
     canvas.setDimensions(200,600,200,600);
     canvas.addShape(new Rectangle(),"rec1");
     SwingView view = new SwingView(canvas);
--- src/cs5004/animator/view/SwingView.java
+++ src/cs5004/animator/view/SwingView.java
@@ -19,22 +19,27 @@ public class SwingView extends JFrame implements View {
     public SwingView(Canvas model) {
         //load in our model.
         this.model = model;
-        this.model.setDimensions(0,500,0,500);
 
-        //Since this object is a JFrame, we can set its attributes by calling its own methods.
+        //The SwingView object is a "JFrame" which will be the entire window that opens. We can use JFrame methods
+        //to configure its attributes.
         setBounds(model.getDimensions()[0],model.getDimensions()[2],model.getDimensions()[1],model.getDimensions()[3]);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setTitle("Easy Animator");
         setLocation(200,200);
 
+        //Create a panel to contain our drawing. This panel will be added to the JFrame.
+        JPanel p = new JPanel();
+        p.setSize(50,50);
+
         //A Canvas (not to be confused with our model by the same name) is a rectangular area which can be drawn on.
         this.background = new java.awt.Canvas();
         this.background.setSize(50,50);
-
         //this.background.setBounds(model.getDimensions()[0],model.getDimensions()[2],50,50);
         this.background.setBackground(new Color(128,0,128));
 
-        this.add(this.background);
+        p.add(this.background);
+
+        this.add(p);
 
         setVisible(true);
     }
