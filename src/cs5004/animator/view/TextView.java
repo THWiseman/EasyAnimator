273aca0751f8d5811bcbbdbb44136af17467ee6d
Tom W <papahsquat@gmail.com>
2021-04-08 08:24:17 -0400

    Moved TextView implementation from CanvasImpl to TextView class

--- src/cs5004/animator/view/TextView.java
+++ src/cs5004/animator/view/TextView.java
@@ -1,51 +1,72 @@
 package cs5004.animator.view;
-
 import java.io.IOException;
 import java.util.Map;
-
 import cs5004.animator.model.Canvas;
+import cs5004.animator.model.Oval;
+import cs5004.animator.model.Rectangle;
 import cs5004.animator.model.Shape;
 
+/**
+ * Text view of the Canvas model. Prints a string description of the canvas provided in
+ * the constructor to the appendable provided in the constructor.
+ */
 public class TextView implements View {
 
   Appendable output;
+  Canvas canvas;
+  Map<String,Shape> shapes;
 
-  public TextView() {
-    this.output = System.out;
+  /**
+   * Constructor for the TextView. Must provided an appendable to output the text to, and
+   * a Canvas from which to draw information.
+   * @param output Appendable object to print text to when the go() method is called.
+   * @param canvas Canvas object to get the information to display from.
+   */
+  public TextView(Appendable output, Canvas canvas) {
+
+    this.output = output;
+    this.canvas = canvas;
+    this.shapes = canvas.getShapeMap();
   }
 
 
+  /**
+   * Returns a string that looks something like this:
+   * Create rectangle R with corner at (200,200), width 50 and height 100, and color {255,255,255}.
+   * Create oval C with center at (500,100), radius 60 and 30, and color {128,0,240}.
+   * @return
+   */
   public String getStringDescription() {
     String allCreates = "";
 
     //this for loop will create a string like "Create rectangle R with corner at (200,200),
-    //width 50 and height 100\n" for each shape and append it to createString.
+    //width 50, height 100, and color {255,255,255}.\n" for each shape and append it to createString.
     for (Map.Entry<String, Shape> e : shapes.entrySet()) {
       String createString = "Create "; //"Create "
       createString += e.getValue().toString() + " "; //"rectangle "
 
-      if (e.getValue().toString() == "rectangle") {
+      if (e.getValue() instanceof Rectangle) {
         //"R with corner at (200,200), "
         createString += String.format("%s with corner at (%d,%d), ", e.getKey(), e.getValue().getPosition(0)[0], e.getValue().getPosition(0)[1]);
         //width 50, height 100, and color.\n"
-        createString += String.format("width %d, height %d, and color %s.\n", e.getValue().getSize(0)[0], e.getValue().getSize(0)[1],e.getValue().getSize(0).toString());
+        createString += String.format("width %d, height %d, and color %s.\n", e.getValue().getSize(0)[0], e.getValue().getSize(0)[1],e.getValue().getColor(0).toString());
       }
-      if (e.getValue().toString() == "oval") {
+      if (e.getValue() instanceof Oval) {
         //"C with center at (500,100), "
         createString += String.format("%s with center at (%d,%d), ", e.getKey(), e.getValue().getPosition(0)[0], e.getValue().getPosition(0)[1]);
         //radius 60 and height 30\n"
-        createString += String.format("radius %d and %d, and color %s.\n", e.getValue().getSize(0)[0], e.getValue().getSize(0)[1],e.getValue().getSize(0).toString());
+        createString += String.format("radius %d and %d, and color %s.\n", e.getValue().getSize(0)[0], e.getValue().getSize(0)[1],e.getValue().getColor(0).toString());
       }
       allCreates += createString;
     }
     allCreates += "\n"; //add extra newline to separate from next section.
 
-    //this for loop will create a string like "R appears at time t=1 and disappears at time t=100\n
+    //this for loop will create a string like "R appears at time t=1 and disappears at time t=100.\n
     //and append it to allAppears.
     String allAppears = "";
     for (Map.Entry<String, Shape> e : shapes.entrySet()) {
       String appearString = "";
-      appearString += e.getKey() + String.format("appears at time t=%d and disappears at time t=%d\n", e.getValue().getAppearTime(), e.getValue().getDisappearTime());
+      appearString += e.getKey() + String.format("appears at time t=%d and disappears at time t=%d.\n", e.getValue().getAppearTime(), e.getValue().getDisappearTime());
       allAppears += appearString;
     }
     allAppears += "\n"; //add extra newline to separate from next section.?.OMRT
@@ -64,17 +85,12 @@ public class TextView implements View {
   }
 
   @Override
-  public void go(Appendable output, Canvas canvas) {
+  public void go() {
     try {
-      output.append(canvas.getStringDescription());
+     output.append(this.getStringDescription());
     } catch (IOException e) {
       throw new IllegalStateException("Appendable error");
     }
-
   }
 
-  @Override
-  public void go(Canvas canvas) {
-
-  }
 }
--- src/cs5004/animator/view/View.java
+++ src/cs5004/animator/view/View.java
@@ -9,18 +9,9 @@ import cs5004.animator.model.Canvas;
 public interface View {
 
   /**
-   * Produces a cs5004.animator.view given valid inputs. Allows user to specify an output destination, which
-   * will be used in the case of textual output.
-   * @param output Destination file or location for the text output of the chosen cs5004.animator.view, if there
-   *               is any.
-   * @param canvas Canvas model from which to draw information.
+   * Causes the view to display the model that it was constructed with. Different types of views will
+   * do different things depending on their responsibility. Refer to the documentation for each view.
    */
-  public void go(Appendable output, Canvas canvas);
-
-  /**
-   * Standard go method that uses System.out for all text output (if there is any).
-   * @param canvas Canvas model from which to draw information.
-   */
-  public void go(Canvas canvas);
+  public void go();
 
 }
