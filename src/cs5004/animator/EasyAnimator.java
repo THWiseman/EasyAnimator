825941d5ac7118be9421053127251d84b54f14a0
Tom W <papahsquat@gmail.com>
2021-04-08 12:14:29 -0400

    started work on the swing view.

--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -3,11 +3,13 @@ package cs5004.animator;
 import cs5004.animator.model.Canvas;
 import cs5004.animator.model.CanvasImpl;
 import cs5004.animator.model.Rectangle;
+import cs5004.animator.util.AnimationReader;
 import cs5004.animator.view.SwingView;
 
 public final class EasyAnimator {
   public static void main(String[] args) {
-    Canvas canvas = new CanvasImpl();
+    Readable file
+    Canvas canvas = AnimationReader.parseFile(toh-3.txt);
     canvas.setDimensions(200,600,200,600);
     canvas.addShape(new Rectangle(),"rec1");
     SwingView view = new SwingView(canvas);
