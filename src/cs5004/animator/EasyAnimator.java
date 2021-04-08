7949f61f24afde89408258a359757a9e10aaad0c
Tom W <papahsquat@gmail.com>
2021-04-08 13:28:24 -0400

    started work on the swing view.

--- src/cs5004/animator/EasyAnimator.java
+++ src/cs5004/animator/EasyAnimator.java
@@ -6,18 +6,20 @@ import cs5004.animator.model.Rectangle;
 import cs5004.animator.util.AnimationReader;
 import cs5004.animator.view.SwingView;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public final class EasyAnimator {
-  public static void main(String[] args) {
-    Readable file;
-    try {
-      file = new FileReader("toh-3.txt");
-    } catch (FileNotFoundException e) {
-      throw new IllegalArgumentException("File not found");
-    }
-    Canvas canvas =  AnimationReader.parseFile(file, new CanvasImpl.Builder);
+  public static void main(String[] args) throws IOException {
+    Path path = Paths.get("/Users/tom/IdeaProjects/EasyAnimator/src/toh-3.txt");
+    Readable reader = Files.newBufferedReader(path);
+
+    Canvas canvas =  AnimationReader.parseFile(reader, new CanvasImpl.Builder());
     canvas.setDimensions(200,600,200,600);
     canvas.addShape(new Rectangle(),"rec1");
     SwingView view = new SwingView(canvas);
