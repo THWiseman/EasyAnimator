1ed937ec0d2159fa76d46ba7b6df9fa9910c10c2
Tom W <papahsquat@gmail.com>
2021-04-09 21:51:06 -0400

    added toh-12.txt for further testing purposes

--- test/cs5004/animator/view/TextViewTest.java
+++ test/cs5004/animator/view/TextViewTest.java
@@ -1,51 +1,33 @@
-/*
+
 package cs5004.animator.view;
 import cs5004.animator.model.*;
+import cs5004.animator.util.AnimationReader;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 import static org.junit.Assert.assertEquals;
 
 
 public class TextViewTest {
-    Canvas defaultCanvas;
-    Canvas small;
-    Canvas big;
-
-    Shape rec1;
-    Shape rec2;
-    Shape ov1;
-    Shape ov2;
-    ColorPattern c1;
-    MovementPattern m1;
-    SizeChangePattern s1;
+    Canvas canvas1;
+    Canvas canvas2;
 
     @Before
-    public void setUp() {
-        c1 = new ColorPattern();
-        c1.change(25,50, new Integer[]{128,128,0});
-       // c1.change(40,70, new Integer[]{200,100,50});
-        m1 = new MovementPattern();
-        s1 = new SizeChangePattern();
-
-        rec1 = new Rectangle();
-        rec2 = new Rectangle(c1, m1, s1, 0, 100);
-        ov1 = new Oval();
-        ov2 = new Oval(c1, m1, s1, 0, 100);
-
-
-        defaultCanvas = new CanvasImpl();
-        small = new CanvasImpl(5);
-        small.addShape(rec1, "1");
-        big = new CanvasImpl(200);
-        big.addShape(rec1, "R1");
-        big.addShape(rec2, "R2");
-        big.addShape(ov1, "O1");
+    public void setUp() throws FileNotFoundException {
+        BufferedReader reader = new BufferedReader((new FileReader("src/testText")));
+        canvas1 = AnimationReader.parseFile(reader, new CanvasImpl.Builder());
+        BufferedReader reader2 = new BufferedReader((new FileReader("src/toh-12.txt")));
+        canvas2 = AnimationReader.parseFile(reader2, new CanvasImpl.Builder());
     }
 
     @Test
     public void testTextView() {
-        TextView text = new TextView(System.out,big);
+        TextView text = new TextView(System.out,canvas1);
         assertEquals("",text.getStringDescription());
     }
 }
-*/
+
